86d7f62a4df72a3fb644dd24c1f35483c893bb20
DonovanPS <donovan.picon@uptc.edu.co>
2024-09-25 21:29:42 -0500

    Solución error de renderizado de window

--- src/components/prediction-praph.jsx
+++ src/components/prediction-praph.jsx
@@ -1,77 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import Chart from 'react-apexcharts';
+import CardHeader from '@mui/material/Card';
+import dynamic from 'next/dynamic';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 export default function ComponentPredictionPraph({ title, subheader, chart, ...other }) {
-  const { labels, colors, series, options } = chart;
+    const { labels, colors, series, options } = chart;
+    const [isClient, setIsClient] = useState(false); 
 
-  // Log series to see their structure
-  useEffect(() => {
+    useEffect(() => {
+        setIsClient(true); // Cambiar a verdadero cuando el componente se monte en el cliente
+    }, []);
 
-  }, [series]);
+    const formatValue = (value) => {
+        if (value === null || value === undefined) {
+            return '';
+        }
+        if (value === 0) {
+            return '0';
+        }
+        const formattedValue = value.toFixed(1);
+        return formattedValue.endsWith('.0') ? formattedValue.slice(0, -2) : formattedValue;
+    };
 
-  const formatValue = (value) => {
-    if (value === null || value === undefined) {
-      return ''; // Devuelve una cadena vacía para valores nulos o indefinidos
+    const chartOptions = {
+        colors,
+        plotOptions: {
+            bar: {
+                columnWidth: '16%',
+            },
+        },
+        fill: {
+            type: series.map((i) => i.fill),
+        },
+        labels,
+        xaxis: {
+            type: 'datetime',
+        },
+        yaxis: {
+            labels: {
+                formatter: formatValue,
+            },
+        },
+        tooltip: {
+            shared: true,
+            intersect: false,
+            y: {
+                formatter: formatValue,
+            },
+        },
+        ...options,
+    };
+
+    if (!isClient) {
+        return null; // Retorna null en el servidor para evitar renderizado
     }
-    if (value === 0) {
-      return '0';
-    }
-    const formattedValue = value.toFixed(1);
-    return formattedValue.endsWith('.0') ? formattedValue.slice(0, -2) : formattedValue;
-  };
 
-  const chartOptions = {
-    colors,
-    plotOptions: {
-      bar: {
-        columnWidth: '16%',
-      },
-    },
-    fill: {
-      type: series.map((i) => i.fill),
-    },
-    labels,
-    xaxis: {
-      type: 'datetime',
-    },
-    yaxis: {
-      labels: {
-        formatter: formatValue,
-      },
-    },
-    tooltip: {
-      shared: true,
-      intersect: false,
-      y: {
-        formatter: formatValue,
-      },
-    },
-    ...options,
-  };
-
-  return (
-    <Card {...other}>
-      <CardHeader title={title} subheader={subheader} />
-      <Box sx={{ p: 3, pb: 1 }}>
-        <Chart
-          dir="ltr"
-          type="line"
-          series={series}
-          options={chartOptions}
-          width="100%"
-          height={364}
-        />
-      </Box>
-    </Card>
-  );
+    return (
+        <Card {...other}>
+            <CardHeader title={title} subheader={subheader} />
+            <Box sx={{ p: 3, pb: 1 }}>
+                <Chart
+                    dir="ltr"
+                    type="line"
+                    series={series}
+                    options={chartOptions}
+                    width="100%"
+                    height={364}
+                />
+            </Box>
+        </Card>
+    );
 }
 
 ComponentPredictionPraph.propTypes = {
-  chart: PropTypes.object.isRequired,
-  subheader: PropTypes.string,
-  title: PropTypes.string,
+    chart: PropTypes.object.isRequired,
+    subheader: PropTypes.string,
+    title: PropTypes.string,
 };
