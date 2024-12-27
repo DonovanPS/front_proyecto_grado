a5a5a0f575732c4b10837d6d64f8c3d2d4c5daa6
DonovanPS <donovan.picon@uptc.edu.co>
2024-12-27 18:54:41 -0500

    Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,46 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Forecasting for Social Good: Predicciones y Análisis de Datos con Modelos Bayesianos",
   description: "Una plataforma interactiva para realizar predicciones de datos mediante modelos bayesianos, enfocada en el bienestar social. Los usuarios pueden cargar sus propios datos o explorar el conjunto de datos de medicamentos proporcionado en el folder 'demo'. Compara predicciones entre diferentes modelos y medicamentos, y analiza las correlaciones entre los elementos seleccionados. Desarrollado con Next.js y Python, este proyecto permite tomar decisiones informadas para promover el bien social a través del análisis avanzado de datos.",
+  
+  // Open Graph
+  openGraph: {
+    title: "Forecasting for Social Good: Predicciones y Análisis de Datos",
+    description: "Plataforma para predicciones de datos con modelos bayesianos enfocados en el bienestar social.",
+    url: "https://monograph.donovanps.site", // URL del sitio
+    siteName: "Forecasting for Social Good",
+    images: [
+      {
+        url: "/images/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "Predicciones de datos con modelos bayesianos"
+      }
+    ]
+  },
+
+  // Twitter Card
+  twitter: {
+    card: "summary_large_image",  
+    site: "@DonovanPicon", 
+    title: "Forecasting for Social Good",
+    description: "Predicciones y análisis de datos con modelos bayesianos para el bienestar social.",
+    image: "/images/og-image.jpg", 
+    creator: "@DonovanPicon", 
+  },
+
+  // Metadatos adicionales (Autor y contexto del proyecto)
+  author: "Donovan Picon",  
+  keywords: "trabajo de grado, modelos bayesianos, predicción de datos, bienestar social, análisis de datos", // Palabras clave relevantes
+  robots: "index, follow", 
+  article: {
+    author: "Donovan Picon", 
+    published_time: "2024-12-01T12:00:00Z",  
+    modified_time: "2024-12-01T12:00:00Z",  
+    section: "Trabajo de Grado",  
+    tag: "Trabajo de Grado, predicciones de datos, modelos bayesianos"
+  }
 };
+
 
 export default function RootLayout({ children }) {
   return (
